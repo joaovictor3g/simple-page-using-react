1a9500a755e911abb1bb9c1f29b7dff8c4dd193e
joaovictor3g <jvdias79797@gmail.com>
2020-02-27 23:01:08 -0300

    updating right now

--- src/Form.js
+++ src/Form.js
@@ -10,11 +10,16 @@ function disparaNoClick() {
 const Form = () => (
     <div className="App" >
       <div id="input-group">
+        <h1>Cadastro de Pessoas</h1>
         Nome:<input id="nome" type="text" placeholder="Digite seu nome"></input>
         RG:<input id="rg" type="text" placeholder="Digite seu rg"></input>
         CPF:<input id="cpf" type="text" placeholder="Digite seu cpf"></input>
         Idade:<input id="idade" type="number" placeholder="Digite sua idade"></input>
-        <button id="salvar" type="submit" onClick={disparaNoClick}>Salvar</button>
+        <div id="button-group">
+          <button id="salvar" type="submit" onClick={disparaNoClick}>Salvar</button>
+          <button id="atualizar" type="submit" onClick={disparaNoClick}>Atualizar</button>
+          <button id="excluir" type="submit" onClick={disparaNoClick}>Excluir</button>
+        </div>
       </div>
     </div>
 );
